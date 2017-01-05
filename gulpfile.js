dcafbc654a603738580ff022acb162878d095f46
Alex Gross <gross.alex@me.com>
2017-01-05 19:45:40 +0100

    important gulp/npm packages installed

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    notify = require('gulp-notify'),
+    less = require('gulp-less'),
+    lessPluginAutoprefix = require('less-plugin-autoprefix'),
+    lessPluginCleanCss = require('less-plugin-clean-css');
