c11e435ffb8e561e7c6d55d373e72f5c20dc1812
Alex Gross <gross.alex@me.com>
2017-01-05 20:08:50 +0100

    Gulp/NPM installed Plugins
    also made the gulp file for the basic tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,27 @@
 var gulp = require('gulp'),
-    uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
     notify = require('gulp-notify'),
     less = require('gulp-less'),
-    lessPluginAutoprefix = require('less-plugin-autoprefix'),
-    lessPluginCleanCss = require('less-plugin-clean-css');
+    LessPluginAutoPrefix = require('less-plugin-autoprefix'),
+    autoprefixPlugin = new LessPluginAutoPrefix({browsers: ["last 2 versions"]}),
+    LessPluginCleanCss = require('less-plugin-clean-css'),
+    cleanCSSPlugin = new LessPluginCleanCss({advanced: true});
+
+gulp.task('styles', function(){
+  gulp.src('./src/less/**/*.less')
+    .pipe(less({
+      plugins: [autoprefixPlugin]
+    }))
+    .pipe(less({
+      plugins: [cleanCSSPlugin]
+    }))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(notify("Compiled CSS: <%= file.relative %>!"));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('./src/less/**/*.less', ['styles']);
+});
+
+
+gulp.task('default', ['styles', 'watch']);
